ad44e55a5009fc4ba925e46ceefb72a46cd9d518
SrJilean <canchicaleandro@gmail.com>
2025-09-26 12:02:36 -0500

    ajustes autodespliegue

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,3 +2,11 @@ output "alb_dns_name" {
   description = "DNS público del Application Load Balancer"
   value       = aws_lb.app.dns_name
 }
+
+output "ecs_service_name" {
+  value = aws_ecs_service.app.name
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.this.name
+}
